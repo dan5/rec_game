14897381c5b246ddddb4db09cee9cb84afcbbe01
dan5 <dan5.ya@gmail.com>
2012-05-08 12:30:28 +0900

    rails new rec_game -m http://railswizard.org/fd9e723977a532a8c53a.rb -J -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecGame::Application.config.secret_token = '7be8fe54d8cb1885e62233366d54e2ab020974a5b9c542214eed81e78cbbd97fd5dc778a6e8b352e61f89bbc69b61e75b4d6854fb82dd2a7183b9397544960c4'
