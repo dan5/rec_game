2411cbcaaca212179e9348dc8edacad2fe2af1d4
dan5 <dan5.ya@gmail.com>
2012-05-11 16:18:53 +0900

    colspan連結

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,2 +1,23 @@
 module ResultsHelper
+  def rowspans_table(rows, colum_name)
+    rowspans = []
+    pre = nil
+    skip_ct = 0
+    rows.each_with_index do |result, i|
+      if skip_ct > 0
+        skip_ct -= 1
+        next
+      end
+      if !pre.blank? and pre == result.send(colum_name)
+        n = 0
+        rows[i - 1, 20].each {|e| e.send(colum_name) == pre ? n += 1 : break }
+        rowspans[i - 1] = n + 1
+        skip_ct = n - 1
+      else
+        rowspans[i] = 1
+        pre = result.send(colum_name)
+      end
+    end
+    rowspans
+  end
 end
