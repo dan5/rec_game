ca69653acc53e2de4fc5dce458de56ce33f41f54
dan5 <dan5.ya@gmail.com>
2012-05-11 16:39:35 +0900

    rowspanを数えるバグを修正

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,21 +1,24 @@
 module ResultsHelper
   def rowspans_table(rows, colum_name)
     rowspans = []
-    pre = nil
     skip_ct = 0
-    rows.each_with_index do |result, i|
+    rows.each_with_index do |row, i|
       if skip_ct > 0
         skip_ct -= 1
         next
       end
-      if !pre.blank? and pre == result.send(colum_name)
+      nxt = if i < rows.size - 1
+              rows[i + 1].send(colum_name)
+            else
+              nil
+            end
+      if nxt == row.send(colum_name)
         n = 0
-        rows[i - 1, 20].each {|e| e.send(colum_name) == pre ? n += 1 : break }
-        rowspans[i - 1] = n + 1
+        rows[i, 20].each {|e| e.send(colum_name) == nxt ? n += 1 : break }
+        rowspans[i] = n
         skip_ct = n - 1
       else
         rowspans[i] = 1
-        pre = result.send(colum_name)
       end
     end
     rowspans
